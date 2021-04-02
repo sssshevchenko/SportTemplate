8ac081657a7bd5e3512fa45399ac910a1ea00e14
sssshevchenko <denshev233@gmail.com>
2021-04-02 21:11:24 +0300

    sport

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,14 @@ $(document).ready(function(){
 		$('.menu').slideToggle();
 		$(this).toggleClass('opened');
 	})
+	
+	$(function() {
+  
+  $('ul.tabs__caption').on('click', 'li:not(.active)', function() {
+    $(this)
+      .addClass('active').siblings().removeClass('active')
+      .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+  });
+  
+});
 });
